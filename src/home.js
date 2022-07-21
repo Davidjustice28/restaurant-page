c6e21514491ca5abd7eb4326f32bf328647ea821
David Justice <davidjustice28@gmail.com>
2022-07-21 01:32:54 -0400

    Add initial files and build initial setup and first modules

---
+++ src/home.js
@@ -0,0 +1,29 @@
+export {homeScreen};
+import Image from "./food.jpg";
+
+function homeScreen() {
+const homeScreen = document.getElementById("container");
+
+const mainImg = document.createElement("img");
+mainImg.setAttribute("src", Image);
+const title = document.createElement("h1");
+title.innerText = "Welcome To My Home Page";
+
+const intro = document.createElement("p");
+intro.innerText = "Hi random person! Thanks for checking out my restaurant project. I tried to use ES6 modules and webpack. Let me know how I did.";
+
+const list = document.createElement("ul");
+list.innerText = "Skills & Tools Used"
+const item1 = document.createElement("li");
+item1.innerText = "Html, CSS";
+const item2 = document.createElement("li");
+item2.innerText = "ES6 Modules & Webpack";
+const item3 = document.createElement("li");
+item3.innerText = "Npm and git";
+list.append(item1, item2, item3);
+
+const subNotes = document.createElement("p");
+subNotes.innerText = "Try clicking each button to see how they switch between modules.";
+
+homeScreen.append(mainImg, title, intro, list,subNotes);
+}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { homeScreen } from "./home";
+import "./styles.css";
+
+//header creation
+
+const header = document.getElementById("header");
+
+const topNav = document.createElement("nav");
+const headerTitle = document.createElement("h1");
+headerTitle.innerText = "Restaurant Page";
+topNav.append(headerTitle);
+
+const bottomNav = document.createElement("nav");
+const button1 = document.createElement("button");
+button1.innerText = "Home";
+const button2 = document.createElement("button");
+button2.innerText = "Menu";
+const button3 = document.createElement("button");
+button3.innerText = "Contact";
+bottomNav.append(button1, button2, button3);
+
+header.append(topNav, bottomNav);
+
+//footer creation
+
+const footer = document.getElementById("footer");
+
+const footerDiv = document.createElement("div");
+const github = document.createElement("a");
+github.setAttribute("href", "https://github.com/Davidjustice28");
+github.innerText = "GitHub Profile;";
+const copyright = document.createElement("p");
+copyright.innerText = "Copyright of David Justice";
+footerDiv.append( copyright, github);
+
+footer.append(footerDiv);
+
+homeScreen();
+
+
+
+
